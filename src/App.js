6499ea90adbdf350704cfa8c9b014215650669f8
Kay N <kaydigital.me@gmail.com>
2019-07-22 22:15:41 -0400

    Added Header Component, links, shop page, contact

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import {Switch, Route} from 'react-router-dom'
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
-import './App.css';
-import './pages/homepage/homepage.styles.scss'
+import "./App.css";
+import "./pages/homepage/homepage.styles.scss";
 
-import HomePage from './pages/homepage/homepage.component.jsx'
-import ShopPage from './pages/shop/shop.component.jsx'
+import HomePage from "./pages/homepage/homepage.component.jsx";
+import ShopPage from "./pages/shop/shop.component.jsx";
+import Header from "./components/header/header.component.jsx";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route exact path='/shop' component={ShopPage} />
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React from "react";
 
-import './collection-item.styles.scss';
+import "./collection-item.styles.scss";
 
 const CollectionItem = ({ id, name, price, imageUrl }) => (
   <div className='collection-item'>
@@ -11,12 +11,12 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
       }}
     />
 
-      <div className='collection-footer'>
-        <span className='name'>{name}</span>
-        <span className='price'>{price}</span>
-      </div>
+    <div className='collection-footer'>
+      <span className='name'>{name}</span>
+      <span className='price'>{price}</span>
+    </div>
 
   </div>
-)
+);
 
 export default CollectionItem;
