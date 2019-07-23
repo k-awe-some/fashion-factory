77711784c41aff45addb421f37f6fc004ceab799
Kay N <kaydigital.me@gmail.com>
2019-07-22 23:02:10 -0400

    Added Sign in & sign up page with SignIn Component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import "./pages/homepage/homepage.styles.scss";
 
 import HomePage from "./pages/homepage/homepage.component.jsx";
 import ShopPage from "./pages/shop/shop.component.jsx";
+import SignInSignUpPage from "./pages/sign-in-sign-up/sign-in-sign-up.component.jsx";
 import Header from "./components/header/header.component.jsx";
 
 function App() {
@@ -14,7 +15,8 @@ function App() {
       <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/shop" component={ShopPage} />
+        <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInSignUpPage} />
       </Switch>
     </div>
   );
